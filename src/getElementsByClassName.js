601cb781aa0324eca7dc136e3daf6b3103957b68
jpcastberg <jpcastberg@gmail.com>
2019-01-02 11:59:22 -0800

    Began pseudocode

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,15 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  //create results array
+  const output = [];
+  //get all html tags in doc
+  const allTags = document.getElementsByTagName('*');
+  //iterate through all tags
+  //allTags
+  //check if tag contains class -> if yes, push to output
+  //check if tag has child elements -> if yes, recursively check child elements^
+  //output results array
+  return output;
 };
