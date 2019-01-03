75fc09539701df693ebf68fa35700c4ebff34144
jpcastberg <jpcastberg@gmail.com>
2019-01-02 17:44:31 -0800

    fixing linting errors, implemented check for arrays and basic data types.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,36 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var output = '';
+
+  if ( typeof obj === 'string' ) {
+    return '"' + obj + '"';
+  }
+
+  if ( typeof obj === 'function' ) {
+    return undefined;
+  }
+  
+  if ( Array.isArray( obj ) ) {
+    output += '[';
+    for (var i = 0; i < obj.length; i++) {
+      output += stringifyJSON( obj[i] );
+      if ( i !== obj.length - 1 ) {
+        output += ',';
+      }
+    }
+    output += ']';
+    return output;
+  }
+
+  //if ( typeof obj === 'object' ) {}
+
+  
+  //check if object is basic data tyoe (str, num, bool, null, undef)
+  //if yes, convert object to str and add to solu
+  //check if obj is array
+  //if yes -> add [, iterate, add string elements, add ]
+  //check if obj literal
+  //same as array, except {}, and :.
+  return ''+ obj;
 };
